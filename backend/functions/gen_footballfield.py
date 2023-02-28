26ecc96a5242de302495e7b5e17b80fd21f3c69a
suyash21101 <suyashb@bu.edu>
2023-02-28 15:46:44 -0500

    hi

--- backend/functions/gen_footballfield.py
+++ backend/functions/gen_footballfield.py
@@ -6,16 +6,17 @@ from datetime import datetime
 
 #SAME AS GEN_VALUATION:
 #update_FF_NAME
-#add_FOOTBALLField
-#deleting Valuations in FootballField
+#add_FOOTBALLField: gets user input elements and creates a row in DB
+# repeat names are allowed 
+#deleting Valuations in FootballField: Delete using name
+# gets userID(key1), ff name, timeseries(key2)
 
-
-def update_FF_NAME(userId, footballFieldTimeSeries,footballFieldName,iex_api_key):
-    
+# get time series from add valuation
+# take naming convention from valuation
+def update_FF_NAME(userId, footballFieldTimeSeries,footballFieldName,iex_api_key): 
     url="https://workshopfinance.iex.cloud/v1/data/workshopfinance/FOOTBALLFIELDS/"+userId+"/"+footballFieldTimeSeries+"?token="+iex_api_key
     footballField=requests.get(url).json()
     footballField[0]['footballFieldName']=footballFieldName
-
     url="https://cloud.iexapis.com/v1/record/workshopfinance/VALUATIONS?duplicateKeyHandling=true&wait=true&token="+iex_api_key
     r=requests.post(url, json=footballField)
     return r
