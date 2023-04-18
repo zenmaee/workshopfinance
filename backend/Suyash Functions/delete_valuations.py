5eccf1e1aeafce1684046bd9d4cf5cbd46e9fbc2
prithi15 <pvganesh@bu.edu>
2023-04-18 17:45:02 -0400

    Merge branch 'main' into prithika

--- backend/Suyash Functions/delete_valuations.py
+++
@@ -1,6 +0,0 @@
-def delete_valuation(footballFieldId, valuationTimeSeries):
-  url = "https://WORKSHOPFINANCE.iex.cloud/v1/data/WORKSHOPFINANCE/VALUATIONS/" + footballFieldId + "/" + valuationTimeSeries + "?&token=sk_29735f4ddf4a47efb27623b229dda54a"
-  import requests
-  r = requests.delete(url)
-  return r
-
--- backend/testing/testing2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import requests
-from time import time
-from datetime import datetime
-
-print(int(3.2))
